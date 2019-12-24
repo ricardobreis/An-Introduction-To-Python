2938c5f2b81584bf014d367a10e9e4c7623c3d2a
Ricardo Reis <ricardo.l.b.reis@gmail.com>
2019-12-24 15:59:17 -0300

    Primeiros passos com pandas.

---
+++ introduction-to-pandas.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+# Build cars DataFrame
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+cars_dict = { 'country':names, 'drives_right':dr, 'cars_per_cap':cpc }
+cars = pd.DataFrame(cars_dict)
+print(cars)
+
+# Definition of row_labels
+row_labels = ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
+
+# Specify row labels of cars
+cars.index = row_labels
+
+# Print cars again
+print(cars)
+
+# Using a CSV file and including index_col
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out cars
+print(cars)
+
+# Print out observation for Japan
+print(cars.loc['JPN'])
+print(cars.iloc[2])
+
+# Print out observations for Australia and Egypt
+print(cars.loc[['AUS','EG']])
+print(cars.iloc[[1,6]])
+
+# Print out drives_right value of Morocco
+print(cars.loc['MOR','drives_right'])
+
+# Print sub-DataFrame
+print(cars.loc[['RU','MOR'],['country','drives_right']])
+
+# Print out drives_right column as Series
+print(cars.loc[:,'drives_right'])
+
+# Print out drives_right column as DataFrame
+print(cars.loc[:,['drives_right']])
+
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars.loc[:,['cars_per_cap', 'drives_right']])
